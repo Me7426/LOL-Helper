847841b3fcb14c535322992863b1738666fbaaf4
Muzi <fake@mail.com>
2025-07-28 01:05:27 +0800

    feat: 再来一局

--- src/components/gameFlow/noneFlow.vue
+++ src/components/gameFlow/noneFlow.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts"></script>
 
+<script setup lang="ts">
+import lcuApi from "@/api/lcuApi";
+</script>
+
 <template>
 	<div class="flex flex-col items-center">
 		<div style="font-size: 100px">🍽️</div>
 		<div style="font-size: 40px">等你开把游戏</div>
+		<div class="mt-4">
+			<button class="btn btn-primary" @click="lcuApi.playAgain">再来一局</button>
+		</div>
 	</div>
 </template>
 
